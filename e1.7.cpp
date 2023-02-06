f701b66b775e262d37e3fc6a2389c12b840d9ef3
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-02-05 19:57:51 -0500

    Create e1.7.cpp

---
+++ e1.7.cpp
@@ -0,0 +1,20 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: 
+Assignment: E1.7
+Wrote a program that prints three items, such as the names of your three best friends 
+or favorite movies, on three separate lines.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Harry" << endl;
+  cout << "Sally" << endl;
+  cout << "Mary" << endl;
+  
+  return 0;
+}
